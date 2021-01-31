cda6415e02410bd2c702be4b186f7321e037fa9c
mip92 <19mip92@gmail.com>
2021-01-31 20:19:24 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from "react"
+import React,{ useState, useEffect } from "react"
 import {Header} from "./components";
-import {Home, Cart} from "./pages";
+import {Home, Cart, Preloader} from "./pages";
 import {Route} from "react-router-dom";
 
 
 function App() {
+    const [pizzas, setPizzas]=useState([null])
+
+    useEffect(()=>{
+        fetch('http://localhost:3000/db.json')
+            .then((res)=>res.json())
+            .then(json=>{
+                setPizzas(json.pizzas);
+            })
+    },[]);
+
     return (
         <div className="wrapper">
             <Header/>
+            {pizzas[0]==null ? <Preloader/>:
             <div className="content">
-                <Route exact path='/'component={Home}/>
-                <Route path='/cart' component={Cart}/>
-            </div>
+                <Route exact path='/'render={ ()=><Home pizzas={pizzas}/>}/>
+                <Route path='/cart' render={()=><Cart/>}/>
+            </div>}
         </div>
     );
 }
